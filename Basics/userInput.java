f0e0695aef9c9f296a6a94a0acd3e3e17d4ebfb5
anjali-2904 <92749087+anjali-2904@users.noreply.github.com>
2023-08-16 22:16:53 +0530

    Update userInput.java

--- Basics/userInput.java
+++ Basics/userInput.java
@@ -1,1 +1,18 @@
-
+import java.util.*;
+import java.lang.*;
+public class Solution {
+    
+    public static void main(String[] args) {
+        // Write your code here
+        Scanner sc=new Scanner(System.in);
+        String str;
+        str=sc.next();
+        char c=str.charAt(0);
+        if(Character.isUpperCase(c))
+        System.out.println("1");
+        else if(Character.isLowerCase(c))
+        System.out.println("0");
+        else
+        System.out.println("-1");
+    }
+}
