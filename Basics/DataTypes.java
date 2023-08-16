9f5b904289c56e32f47c3d975af7d910d5e06ec6
anjali-2904 <92749087+anjali-2904@users.noreply.github.com>
2023-08-16 22:18:11 +0530

    Update DataTypes.java

--- Basics/DataTypes.java
+++ Basics/DataTypes.java
@@ -1,1 +1,17 @@
 
+import java.util.*;
+public class Solution {
+    public static int dataTypes(String type) {
+        // Write your code here
+        Scanner sc=new Scanner(System.in);
+        //String str=sc.next();
+        int res=0;
+        if(type.equals("Integer") ||type.equals("Float"))
+        res=4;
+        else if(type.equals("Long")|| type.equals("Double"))
+        res=8;
+        else if(type.equals("Character"))
+        res=1;
+        return res;
+    }
+}
