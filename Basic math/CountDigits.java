ef55d826d8147319ccec08ca1df9d966cbd7b403
anjali-2904 <92749087+anjali-2904@users.noreply.github.com>
2023-08-18 15:49:01 +0530

    Update CountDigits.java

--- Basic math/CountDigits.java
+++ Basic math/CountDigits.java
@@ -1,22 +1,16 @@
 import java.util.*;
-
-import javax.print.DocFlavor.STRING;
-public class Main {
-	
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		int num=sc.nextInt();
-		int even=0,odd=0;
-		while(num>0){
-			int rem=0;
-			rem=num%10;
-			if(rem%2==0)
-			even+=rem;
-			else
-			odd+=rem;
-			num/=10;
-		}
-		System.out.println(even+" "+odd);
-
-	}
+public class Solution {
+    public static int countDigits(int n){
+        // Write your code here.
+    int original=n,count=0;
+        for(;n>0;n/=10)
+	{
+            if(n%10==0)
+            continue;
+            else if(original%(n%10)==0){
+                count++;
+            }
+        }
+        return count;
+    }
 }
