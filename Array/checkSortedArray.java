4f651e69ce25cf9e54809e47bdea00584c8a925a
anjali-2904 <92749087+anjali-2904@users.noreply.github.com>
2023-08-25 16:32:09 +0530

    Create checkSortedArray.java

---
+++ Array/checkSortedArray.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Solution {
+    public static int isSorted(int n, int []a) {
+       int res=0;
+       for(int i=0;i<n-1;i++){
+           if(a[i]>a[i+1])
+           {
+               res=0;
+               break;
+           }
+           else
+           res=1;
+       }
+    return res;
+    }
+}
