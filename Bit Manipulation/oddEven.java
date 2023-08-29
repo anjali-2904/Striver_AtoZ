fcd64c754f6ddd587e5ba43688e22a10be698f2a
anjali-2904 <92749087+anjali-2904@users.noreply.github.com>
2023-08-29 22:12:37 +0530

    Create oddEven.java

---
+++ Bit Manipulation/oddEven.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public static String oddEven(int N){
+        // Write your code here.
+        String result="";
+        if(N%2==0){
+            result="even";
+        }
+        else
+        result="odd";
+        return result;
+    }
+}
