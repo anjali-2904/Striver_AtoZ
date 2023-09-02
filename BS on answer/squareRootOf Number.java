4ef74de42dfd575d73e932dfb77d93e58781f36a
anjali-2904 <92749087+anjali-2904@users.noreply.github.com>
2023-09-02 18:11:59 +0530

    Update squareRootOf Number.java

--- BS on answer/squareRootOf Number.java
+++ BS on answer/squareRootOf Number.java
@@ -4,9 +4,6 @@ import java.io.*;
 public class Solution {
 
 	public static int sqrtN(long N) {
-		/*
-		 * Write your code here
-		 */
 		 int res;
 		 res=(int)Math.floor(Math.sqrt(N));
 		 return res;
