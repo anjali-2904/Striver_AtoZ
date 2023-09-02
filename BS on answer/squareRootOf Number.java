40ac0fdd442d89f13569f4028444738720ce80e5
anjali-2904 <92749087+anjali-2904@users.noreply.github.com>
2023-09-02 18:11:29 +0530

    Create squareRootOf Number.java

---
+++ BS on answer/squareRootOf Number.java
@@ -0,0 +1,14 @@
+import java.util.* ;
+import java.io.*; 
+
+public class Solution {
+
+	public static int sqrtN(long N) {
+		/*
+		 * Write your code here
+		 */
+		 int res;
+		 res=(int)Math.floor(Math.sqrt(N));
+		 return res;
+	}
+}
