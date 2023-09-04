27a4f391a57325c33bfc103fc2c833a82aa2ae83
anjali-2904 <92749087+anjali-2904@users.noreply.github.com>
2023-09-04 18:49:03 +0530

    Create SumOfAllDivisors.java

---
+++ Basic math/SumOfAllDivisors.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Solution {
+    public static int sumOfAllDivisors(int n){
+        // Write your code here.
+        int sum=0;
+       
+        for( int i=1;i<=n;i++){
+            for(int j=1;j<=(int)Math.sqrt(i);j++){
+                if(i%j==0){
+                if(i/j==j)
+                sum+=j;
+                else
+                sum+=j+i/j;
+                }
+            }
+        }
+        
+        return sum;
+    }
+}
