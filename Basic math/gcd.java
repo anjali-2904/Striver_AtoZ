ec1742aee15c2b9db8ee13fbc5194609c0ff859b
anjali-2904 <92749087+anjali-2904@users.noreply.github.com>
2023-09-04 18:48:17 +0530

    Update gcd.java

--- Basic math/gcd.java
+++ Basic math/gcd.java
@@ -2,12 +2,16 @@ import java.util.*;
 public class Solution {
     public static int calcGCD(int n, int m){
         // Write your code here.
-        int min=0,res=1;
-        min=Math.min(n, m);
-        for(int i=2;i<=min;i++){
-            if(n%i==0 && m%i==0)
-            res=i;
-           
+        int res=-1;
+        while(n>0&& m>0){
+            if(n>m)
+            n=n%m;
+            else
+            m=m%n;
+            if(n==0)
+            res=m;
+            else
+            res=n;
         }
         return res;
     }
