45cada64fabf8139f09b3527882e8006ce5d51c8
anjali-2904 <92749087+anjali-2904@users.noreply.github.com>
2023-09-06 17:00:00 +0530

    Create DivideTwoInteger.java

---
+++ Bit Manipulation/DivideTwoInteger.java
@@ -0,0 +1,9 @@
+public class Solution{
+    public static int divideTwoInteger(int dividend, int divisor) {
+        // Write your code here.
+        int res;
+        
+        res=dividend/divisor;
+        return res;
+    }
+}
