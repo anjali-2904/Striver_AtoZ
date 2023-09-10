6203e325d179230ffc2e7d483cc51aca041f936c
anjali-2904 <92749087+anjali-2904@users.noreply.github.com>
2023-09-10 13:21:14 +0530

    Update Print_1ToN.java

--- Basic Recursion/Print_1ToN.java
+++ Basic Recursion/Print_1ToN.java
@@ -1,1 +1,15 @@
 
+import java.util.*;
+public class Solution {
+    public static int[] printNos(int x) {
+        int arr[]=new int[x];
+        printNo(arr,0,x,1);
+        return arr;       
+    }
+    public static void printNo(int arr[],int i,int x,int j){
+        if(x==0)
+        return ;
+        arr[i]=j;
+        printNo(arr, i+1, x-1,j+1);
+    }
+}
