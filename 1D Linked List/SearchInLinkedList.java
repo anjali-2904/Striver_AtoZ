b2d4a14533885e83ca0cca132559d1fbc181bc3b
anjali-2904 <92749087+anjali-2904@users.noreply.github.com>
2023-09-22 14:59:42 +0530

    Create SearchInLinkedList.java

---
+++ 1D Linked List/SearchInLinkedList.java
@@ -0,0 +1,45 @@
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+    
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+     }
+    
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+     }
+    
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+     }
+ }
+
+ *****************************************************************/
+
+
+public class Solution
+{
+    public static int searchInLinkedList(Node head, int k)
+    {
+        // Write Your Code Here.
+        int res=0;
+        while(head!=null){
+            if(head.data==k){
+            res=1;
+            break;}
+            head=head.next;
+        }
+        return res;
+    }
+}
